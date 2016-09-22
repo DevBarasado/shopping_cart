b625b461d641907c1a29835d8f2194465e717875
DevBarasado <devbarasado@gmail.com>
2016-09-22 05:40:55 -0500

    Update ProductTableSeeder.php

--- database/seeds/ProductTableSeeder.php
+++ database/seeds/ProductTableSeeder.php
@@ -12,46 +12,46 @@ class ProductTableSeeder extends Seeder
     public function run()
     {
         $product = new \App\Product([
-            'imagePath' => 'http://www.barasado.com/resources/uploads/sun32.jpg',
-            'title' => 'Tales of Berseria',
-            'description' => 'PS4',
+            'imagePath' => 'http://vanisbakery.com/wp-content/uploads/2016/09/sun37.jpg',
+            'title' => 'Zelda',
+            'description' => 'Breath of the wild',
             'price' => 20
         ]);
         $product->save();
         $product = new \App\Product([
-            'imagePath' => 'http://www.barasado.com/resources/uploads/professional-video-camera.jpg',
+            'imagePath' => 'http://vanisbakery.com/wp-content/uploads/2016/09/professional-video-camera-300x290.jpg',
             'title' => 'Sonny',
             'description' => 'Professional Camera with high resolutions!',
-            'price' => 10
+            'price' => 1200
         ]);
         $product->save();
         $product = new \App\Product([
-            'imagePath' => 'http://www.barasado.com/resources/uploads/sun45.jpg',
-            'title' => 'Addidas Running shoe',
-            'description' => 'Great for walking and running',
+            'imagePath' => 'http://vanisbakery.com/wp-content/uploads/2016/09/6-300x270.jpg',
+            'title' => 'Dell Laptop',
+            'description' => 'Purple and great for high school and college students',
             'price' => 79
         ]);
         $product->save();
         $product = new \App\Product([
-            'imagePath' => 'http://www.barasado.com/resources/uploads/sun1.jpg',
+            'imagePath' => 'http://vanisbakery.com/wp-content/uploads/2016/09/sun1-300x300.jpg',
             'title' => 'Elegant dress for special occassion',
             'description' => 'Great for wedding occassions and that special day!',
-            'price' => 250
+            'price' => 89
         ]);
         $product->save();
         $product = new \App\Product([
-            'imagePath' => 'http://www.barasado.com/resources/uploads/sun58.jpg',
-            'title' => 'HERP4 ',
+            'imagePath' => 'http://vanisbakery.com/wp-content/uploads/2016/09/sun58.jpg',
+            'title' => 'HERO4 ',
             'description' => 'With high resolutions and motion detectors',
             'price' => 500
         ]);
         $product->save();
         
         $product = new \App\Product([
-            'imagePath' => 'http://www.barasado.com/resources/uploads/sun50.jpg',
-            'title' => 'Canon',
+            'imagePath' => 'http://vanisbakery.com/wp-content/uploads/2016/09/Canon-EOS-Rebel-T3i.jpg',
+            'title' => 'Canon-Eos-Rebel-T3i',
             'description' => 'Perfect for safari travellers and site seeing vacations!',
-            'price' => 20
+            'price' => 750
         ]);
         $product->save();
     }
